488f50fc7e38b0c7ec969bbaeca575321f211111
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-03-28 17:33:15 +0200

    Added a simple open scene graph viewer.

--- Makefile
+++ Makefile
@@ -1,6 +1,6 @@
 OBJECTS = $(patsubst %.c,%.o,$(wildcard *.cpp))
 #CFLAGS = $(shell pkg-config --cflags bullet)
-LIBS = -lstdc++ -lBulletDynamics -lBulletCollision
+LIBS = -lstdc++ -lBulletDynamics -lBulletCollision -losgViewer
 
 all: 4wd
 
--- program.cpp
+++ program.cpp
@@ -1,4 +1,5 @@
 #include <btBulletDynamicsCommon.h>
+#include <osgViewer/Viewer>
 #include <iostream>
 #include <string>
 
@@ -6,5 +7,8 @@ int main(int argc, char *argv[]) {
 	std::cout << "Starting 4WD" << std::endl;
 	btDefaultCollisionConfiguration* collisionConfiguration = new btDefaultCollisionConfiguration();
 
+	osgViewer::Viewer viewer;
+	viewer.setUpViewInWindow(10, 30, 600, 500);
+	viewer.run();
 	return 0;
 }
