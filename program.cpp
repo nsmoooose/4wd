0c798c6c7667dcfaa442d5f677de4142099f559e
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-04-04 20:30:45 +0200

    Some more parameters.

--- program.cpp
+++ program.cpp
@@ -48,13 +48,13 @@ osg::MatrixTransform *create_box(osg::Vec3 size) {
 }
 
 void createWorld(osg::Group *parent, btDynamicsWorld *dynamicsWorld) {
-	osg::ref_ptr<osg::MatrixTransform> ground = create_box(osg::Vec3(10, 10, 0.01));
+	osg::ref_ptr<osg::MatrixTransform> ground = create_box(osg::Vec3(10, 10, 0.1));
 	parent->addChild(ground.get());
 
     btRigidBody::btRigidBodyConstructionInfo rb(
 		btScalar(0.0), /* mass */
 		new MotionState(ground.get()),
-		new btBoxShape(btVector3(5, 5, 0.005)),
+		new btBoxShape(btVector3(10, 10, 0.1)),
 		btVector3(0, 0, 0)); /* inertia */
 	dynamicsWorld->addRigidBody(new btRigidBody(rb));
 
@@ -62,15 +62,34 @@ void createWorld(osg::Group *parent, btDynamicsWorld *dynamicsWorld) {
 	osg::ref_ptr<osg::MatrixTransform> box = create_box(osg::Vec3(3, 3, 3));
 	osg::Matrix matrix;
 	matrix.makeRotate(45.0, osg::Vec3(1.0, 0.0, 0.0));
-	matrix.setTrans(0.0, 0.0, 8.0);
+	matrix.setTrans(0.0, 0.0, 14.0);
 	box->setMatrix(matrix);
 	parent->addChild(box.get());
 
+	btBoxShape *shape = new btBoxShape(btVector3(3, 3, 3));
+	btVector3 inertia(1, 1, 1);
     btRigidBody::btRigidBodyConstructionInfo box_rb(
 		btScalar(1.0), /* mass */
 		new MotionState(box.get()),
-		new btBoxShape(btVector3(1.5, 1.5, 1.5)),
-		btVector3(0, 0, 0)); /* inertia */
+		shape,
+		inertia); /* inertia */
+	dynamicsWorld->addRigidBody(new btRigidBody(box_rb));
+
+
+
+	box = create_box(osg::Vec3(7, 3, 3));
+	matrix.makeRotate(5.0, osg::Vec3(1.0, 0.0, 0.0));
+	matrix.setTrans(0.0, 0.0, 40.0);
+	box->setMatrix(matrix);
+	parent->addChild(box.get());
+
+	shape = new btBoxShape(btVector3(7, 3, 3));
+	inertia = btVector3(1, 1, 1);
+    box_rb = btRigidBody::btRigidBodyConstructionInfo(
+		btScalar(3.0), /* mass */
+		new MotionState(box.get()),
+		shape,
+		inertia); /* inertia */
 	dynamicsWorld->addRigidBody(new btRigidBody(box_rb));
 }
 
@@ -82,9 +101,10 @@ int main(int argc, char *argv[]) {
     btConstraintSolver *solver = new btSequentialImpulseConstraintSolver();
     btVector3 worldAabbMin(-10000, -10000, -10000);
     btVector3 worldAabbMax(10000, 10000, 10000);
-    btBroadphaseInterface *inter = new btAxisSweep3(worldAabbMin, worldAabbMax, 1000);
+    /* btBroadphaseInterface *inter = new btAxisSweep3(worldAabbMin, worldAabbMax, 1000); */
+    btBroadphaseInterface *inter = new btDbvtBroadphase();
     btDynamicsWorld *dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, inter, solver, collisionConfiguration);
-    dynamicsWorld->setGravity(btVector3(0, 0, -10 ));
+    dynamicsWorld->setGravity(btVector3(0, 0, -10));
 
 	osgViewer::Viewer viewer;
 	viewer.setUpViewInWindow(10, 30, 600, 500);
