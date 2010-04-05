34e243252c46b1a907871ee5d2ead1a0d63d9a44
Henrik Nilsson <henrik.nilsson@tvaaker.se>
2010-04-05 17:09:37 +0200

    Some refactoring.

--- program.cpp
+++ program.cpp
@@ -1,6 +1,7 @@
 #include <btBulletDynamicsCommon.h>
 #include <osg/ShapeDrawable>
 #include <osg/MatrixTransform>
+#include <osgDB/ReadFile>
 #include <osgGA/TrackballManipulator>
 #include <osgViewer/Viewer>
 #include <osgViewer/ViewerEventHandlers>
@@ -34,63 +35,63 @@ private:
 	osg::ref_ptr<osg::MatrixTransform> m_transform;
 };
 
-osg::MatrixTransform *create_box(osg::Vec3 size) {
-	osg::ref_ptr<osg::Box> box = new osg::Box();
-    box->setHalfLengths(size);
+/*
+   osg::Cylinder(const osg::Vec3 &center, float radius, float height)
+   btCylinderShape(const btVector3 &halfExtents);
 
+   osg::Sphere(const osg::Vec3 &center, float radius)
+   btMultiSphereShape(const btVector3 *positions, const btScalar *radi, int	numSpheres)
+*/
+
+void create_osg_box(osg::MatrixTransform *transform, osg::Vec3 size) {
+	osg::ref_ptr<osg::Box> box = new osg::Box();
+	box->setHalfLengths(size);
 	osg::ref_ptr<osg::ShapeDrawable> shape = new osg::ShapeDrawable(box);
 	osg::ref_ptr<osg::Geode> geode = new osg::Geode();
-    geode->addDrawable(shape.get());
-
-	osg::ref_ptr<osg::MatrixTransform> transform = new osg::MatrixTransform();
-    transform->addChild(geode.get());
-    return transform.release();
+	geode->addDrawable(shape.get());
+	transform->addChild(geode.get());
 }
 
-void createWorld(osg::Group *parent, btDynamicsWorld *dynamicsWorld) {
-	osg::ref_ptr<osg::MatrixTransform> ground = create_box(osg::Vec3(10, 10, 0.1));
-	parent->addChild(ground.get());
+void create_box_model(osg::MatrixTransform *transform, btDynamicsWorld *dynamicsWorld,
+				osg::Vec3 size, btVector3 inertia, btScalar mass) {
+	create_osg_box(transform, size);
 
+	btBoxShape *shape = new btBoxShape(btVector3(size._v[0], size._v[1], size._v[2]));
     btRigidBody::btRigidBodyConstructionInfo rb(
-		btScalar(0.0), /* mass */
-		new MotionState(ground.get()),
-		new btBoxShape(btVector3(10, 10, 0.1)),
-		btVector3(0, 0, 0)); /* inertia */
+		mass,
+		new MotionState(transform),
+		shape,
+		inertia);
 	dynamicsWorld->addRigidBody(new btRigidBody(rb));
+}
 
+void createWorld(osg::Group *worldNode, btDynamicsWorld *dynamicsWorld) {
+#if 0
+	osg::ref_ptr<osg::Node> terrain = osgDB::readNodeFile("puget.ive");
+	if(terrain) {
+		worldNode->addChild(terrain.get());
+	}
+#endif
 
-	osg::ref_ptr<osg::MatrixTransform> box = create_box(osg::Vec3(3, 3, 3));
+	osg::ref_ptr<osg::MatrixTransform> ground = new osg::MatrixTransform();
+	create_box_model(ground.get(), dynamicsWorld, osg::Vec3(10, 10, 0.1), btVector3(0,0,0), btScalar(0));
+	worldNode->addChild(ground.get());
+
+	osg::ref_ptr<osg::MatrixTransform> box = new osg::MatrixTransform();
 	osg::Matrix matrix;
 	matrix.makeRotate(45.0, osg::Vec3(1.0, 0.0, 0.0));
 	matrix.setTrans(0.0, 0.0, 14.0);
 	box->setMatrix(matrix);
-	parent->addChild(box.get());
+	create_box_model(box.get(), dynamicsWorld, osg::Vec3(3, 3, 3), btVector3(1, 1, 1), btScalar(1.0));
+	worldNode->addChild(box.get());
 
-	btBoxShape *shape = new btBoxShape(btVector3(3, 3, 3));
-	btVector3 inertia(1, 1, 1);
-    btRigidBody::btRigidBodyConstructionInfo box_rb(
-		btScalar(1.0), /* mass */
-		new MotionState(box.get()),
-		shape,
-		inertia); /* inertia */
-	dynamicsWorld->addRigidBody(new btRigidBody(box_rb));
-
-
-
-	box = create_box(osg::Vec3(7, 3, 3));
+	box = new osg::MatrixTransform();
+	matrix = osg::Matrix();
 	matrix.makeRotate(5.0, osg::Vec3(1.0, 0.0, 0.0));
 	matrix.setTrans(0.0, 0.0, 40.0);
 	box->setMatrix(matrix);
-	parent->addChild(box.get());
-
-	shape = new btBoxShape(btVector3(7, 3, 3));
-	inertia = btVector3(1, 1, 1);
-    box_rb = btRigidBody::btRigidBodyConstructionInfo(
-		btScalar(3.0), /* mass */
-		new MotionState(box.get()),
-		shape,
-		inertia); /* inertia */
-	dynamicsWorld->addRigidBody(new btRigidBody(box_rb));
+	create_box_model(box.get(), dynamicsWorld, osg::Vec3(7, 3, 3), btVector3(1, 1, 1), btScalar(3.0));
+	worldNode->addChild(box.get());
 }
 
 int main(int argc, char *argv[]) {
@@ -101,7 +102,6 @@ int main(int argc, char *argv[]) {
     btConstraintSolver *solver = new btSequentialImpulseConstraintSolver();
     btVector3 worldAabbMin(-10000, -10000, -10000);
     btVector3 worldAabbMax(10000, 10000, 10000);
-    /* btBroadphaseInterface *inter = new btAxisSweep3(worldAabbMin, worldAabbMax, 1000); */
     btBroadphaseInterface *inter = new btDbvtBroadphase();
     btDynamicsWorld *dynamicsWorld = new btDiscreteDynamicsWorld(dispatcher, inter, solver, collisionConfiguration);
     dynamicsWorld->setGravity(btVector3(0, 0, -10));
@@ -114,6 +114,8 @@ int main(int argc, char *argv[]) {
 
 	viewer.setSceneData(root.get());
     viewer.addEventHandler(new osgViewer::StatsHandler);
+    viewer.addEventHandler(new osgViewer::HelpHandler);
+    viewer.addEventHandler(new osgViewer::WindowSizeHandler);
     viewer.setCameraManipulator(new osgGA::TrackballManipulator());
 
     double prevSimTime = viewer.getFrameStamp()->getSimulationTime();
